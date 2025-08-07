f67839f9f34d56aa4af109892a862a26263d2f44
D.Venancio <venanciomagalhaesd@gmail.com>
2025-08-06 23:27:43 -0300

    adicionando estrutura básica do docker

---
+++ Dockerfile
@@ -0,0 +1,42 @@
+FROM php:8.4-fpm-alpine
+
+WORKDIR /var/www/html
+
+RUN apk add --no-cache \
+    bash \
+    git \
+    unzip \
+    curl \
+    libzip-dev \
+    libpng-dev \
+    libxml2-dev \
+    zlib-dev \
+    oniguruma-dev \
+    curl-dev \
+    shadow \
+    autoconf \
+    gcc \
+    g++ \
+    make \
+    musl-dev
+
+RUN docker-php-ext-install \
+    zip \
+    pdo \
+    pdo_mysql \
+    mbstring \
+    fileinfo \
+    xml \
+    dom \
+    curl
+
+RUN pecl install redis \
+    && docker-php-ext-enable redis
+
+COPY --from=node:lts-alpine /usr/local/ /usr/local/
+
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+
+EXPOSE 9000
+
+CMD ["php-fpm"]
---
+++ docker/scripts/entrypoint.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+set -e
+
+echo "Configurando Git seguro..."
+git config --global --add safe.directory /var/www/html
+
+echo "Instalando dependências..."
+if [ ! -d vendor ]; then
+  if command -v composer >/dev/null; then
+    composer install --no-interaction
+  else
+    echo "❌ Composer não encontrado"
+    exit 1
+  fi
+fi
+
+if [ ! -d node_modules ]; then
+  if command -v npm >/dev/null; then
+    npm install
+  else
+    echo "❌ npm não encontrado"
+    exit 1
+  fi
+fi
+
+echo "Compilando assets..."
+if ! npm run build; then
+  echo "❌ Falha ao compilar assets"
+  exit 1
+fi
+
+echo "Rodando migrações..."
+if ! php artisan migrate; then
+  echo "❌ Migrações falharam"
+  exit 1
+fi
+
+echo "Limpando cache e otimizando..."
+php artisan cache:clear
+php artisan optimize:clear
+
+
+chown -R www-data:www-data storage bootstrap/cache
+chmod -R 777 storage bootstrap/cache
+
+echo "✅ Pronto. Iniciando o PHP-FPM..."
+exec php-fpm
